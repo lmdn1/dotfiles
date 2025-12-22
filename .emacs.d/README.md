11c68647cf473c6b62e74c37603f143f5a56153d
lmdn1 <l@lmdn.cc>
2025-12-22 03:31:19 +0000

    Added early nvim config, subject to change.
    Moving away from emacs, nvim config is mostly an amalgamation of my
    existing .vimrc and .emacs.d

---
+++ .emacs.d/README.md
@@ -0,0 +1,5 @@
+# emacs
+
+Basically deprecated, currently (finally getting around to) moving to nvim.
+
+Just keeping this here in case something goes horribly wrong.
---
+++ nvim/init.lua
@@ -0,0 +1,278 @@
+-- init.lua
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- UI --
+vim.opt.laststatus = 1
+vim.opt.ruler = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.visualbell = false
+vim.opt.errorbells = false
+vim.opt.ttyfast = true
+vim.opt.lazyredraw = false
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+
+-- Behavior --
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 5
+vim.opt.sidescrolloff = 8
+vim.opt.timeout = true
+vim.opt.timeoutlen = 250
+vim.opt.updatetime = 250
+vim.opt.wildmenu = true
+vim.opt.wildmode = { "longest:full", "full" }
+vim.opt.backspace = { "indent", "eol", "start" }
+vim.opt.hidden = true
+
+-- Search --
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.showmatch = true
+
+-- Misc --
+vim.opt.clipboard = "unnamedplus"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("data").."/undodir"
+-- Not needed?
+-- vim.opt.syntax = "on"
+
+-- Plugin manager --
+local lazypath = vim.fn.stdpath("data").."/lazy/lazy.nvim"
+local lazygitpath = "https://github.com/folke/lazy.nvim.git"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    lazygitpath, "--branch=stable", lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins --
+require("lazy").setup({
+  spec = {
+    {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      opts = {
+        highlight = { enable = true },
+        indent = { enable = true },
+      },
+      ensure_installed = {
+        "python", "rust", "go", "c", "cpp",
+        "javascript", "typescript", "lua", "bash",
+      },
+    },
+    { "neovim/nvim-lspconfig" },
+    { "onsails/lspkind.nvim" },
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    {
+      "mason-org/mason-lspconfig.nvim",
+      dependencies = {
+        {"mason-org/mason.nvim", opts = {}},
+        "neovim/nvim-lspconfig",
+      },
+      opts = {},
+    },
+    {
+      "nvim-telescope/telescope.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-fzf-native.nvim",
+      },
+      opts = {
+        defaults = {
+          border = false,
+          layout_strategy = "bottom_pane",
+          sorting_strategy = "ascending",
+          layout_config = {
+            height = 15,
+            prompt_position = "bottom",
+          },
+          mappings = {
+            i = { ["<C-g>"] = "close" },
+            n = { ["<C-g>"] = "close" },
+          },
+          config = function()
+            require("telescope").load_extension("fzf")
+          end,
+        }
+      },
+    },
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      opts = {
+        window = {
+          width = 25,
+          mappings = {
+            [ "/" ] = "noop",
+          },
+        },
+      },
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        "nvim-tree/nvim-web-devicons",
+      },
+    },
+    {
+      "stevearc/oil.nvim", opts = {
+        default_file_explorer = false,
+        skip_confirm_for_simple_edits = true,
+      },
+    },
+    { "tpope/vim-surround" },
+    { "tpope/vim-commentary" },
+    { "tpope/vim-fugitive" },
+    { "tpope/vim-sleuth" },
+    { "lewis6991/gitsigns.nvim", opts = {} },
+    { "tribela/vim-transparent" },
+    { "mbbill/undotree" },
+    { "windwp/nvim-autopairs", opts = {}},
+    { "axvr/photon.vim" },
+  },
+  defaults = {
+    lazy = false,
+    version = false,
+    news = false,
+  },
+  install = {},
+  checker = { enabled = false },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      }
+    }
+  }
+})
+
+vim.cmd("colorscheme photon")
+
+-- Completions --
+local cmp = require('cmp')
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    [ '<CR>' ] = cmp.mapping.confirm({ select = true }),
+    [ '<Tab>' ] = cmp.mapping.select_next_item(),
+    [ '<S-Tab>' ] = cmp.mapping.select_prev_item(),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+    { name = 'path' },
+  }),
+  formatting = {
+    format = require('lspkind').cmp_format({
+      mode = "text",
+      maxwidth = 40,
+      symbol_map = {},
+    })
+  },
+  window = {
+    completion = {
+      max_height = 15,
+    }
+  },
+})
+
+-- Keymaps --
+local opts = { noremap = true, silent = false }
+local keymaps = {
+  {"n", "<leader>h", ":noh<CR>"},
+
+  -- Don't judge me
+  { "n", "<Up>", "k" },
+  { "n", "<Down>", "j" },
+  { "n", "<Left>", "h" },
+  { "n", "<Right>", "l" },
+
+  { "n", "<leader>nt", ":tabnew<CR>" },
+  { "n", "<leader>nn", ":tabnext<CR>" },
+  { "n", "<leader>nb", ":tabprev<CR>" },
+  { "n", "<leader>-", ":split<CR>" },
+  { "n", "<leader>=", ":vsplit<CR>" },
+  { "n", "<leader>wq", ":close<CR>" },
+
+  -- Seems redundant but arrows don't work
+  -- otherwise, despite being bound to hjkl
+  { "n", "<C-h>", "<C-w>h" },
+  { "n", "<C-Left>", "<C-w>h" },
+  { "n", "<C-j>", "<C-w>j" },
+  { "n", "<C-Down>", "<C-w>j" },
+  { "n", "<C-k>", "<C-w>k" },
+  { "n", "<C-Up>", "<C-w>k" },
+  { "n", "<C-l>", "<C-w>l" },
+  { "n", "<C-Right>", "<C-w>l" },
+  { "n", "<S-Left>", ":vertical resize -2<CR>" },
+  { "n", "<S-Right>", ":vertical resize +2<CR>" },
+  { "n", "<S-Up>", ":resize +2<CR>" },
+  { "n", "<S-Down>", ":resize -2<CR>" },
+
+  { "n", "<leader>ft", ":botright terminal<CR>" },
+  { "t", "<Esc>", [[<C-\><C-n>]] },
+
+  { "n", "gd", vim.lsp.buf.definition },
+  { "n", "gy", vim.lsp.buf.type_definition },
+  { "n", "gi", vim.lsp.buf.implementation },
+  { "n", "gr", vim.lsp.buf.references },
+
+  { "n", "<leader>cr", vim.lsp.buf.rename },
+  { "n", "<leader>ca", vim.lsp.buf.code_action },
+  { "n", "<leader>cf", vim.lsp.buf.format },
+
+  { "n", "K", vim.lsp.buf.hover },
+  { "n", "[g", vim.diagnostic.goto_prev },
+  { "n", "]g", vim.diagnostic.goto_next },
+  { "n", "<leader>xx", vim.diagnostic.setloclist },
+
+  { "n", "<leader>ff", ":Telescope find_files<CR>" },
+  { "n", "<leader>fg", ":Telescope live_grep<CR>" },
+  { "n", "<leader>bb", ":Telescope buffers<CR>" },
+
+  { "n", "<leader>gs", ":Telescope git_status<CR>" },
+  { "n", "<leader>gc", ":Git commit<CR>" },
+  { "n", "<leader>gv", ":Gvdiffsplit<CR>" },
+
+  { "n", "<leader>u", ":UndotreeToggle<CR>" },
+  { "n", "<leader>e", ":Neotree toggle<CR>" },
+  { "n", "<leader>d", ":Oil<CR>" },
+
+  -- Shhhhhhhh
+  { { "n", "v", "i" }, "<C-x><C-s>", "<Esc><cmd>w<CR>" },
+  { { "n", "v", "i" }, "<C-x><C-c>", "<Esc><cmd>qa<CR>" },
+  { { "n", "v", "i", "t", "c" }, "<C-g>", "<Esc>" },
+  { { "n", "v", "i" }, "<M-x>", "<Esc><cmd>Telescope commands<CR>" },
+
+}
+for _,v in ipairs(keymaps) do
+  vim.keymap.set(v[1], v[2], v[3], opts)
+end
+
+-- Autocmd --
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focus = false })
+  end
+})
